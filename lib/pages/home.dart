b1b0820a02eb78681e8e6a40c55e08a9af634ba7
zero0dayali <beeunik0@gmail.com>
2021-08-19 16:54:57 +0700

    add search bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,52 +7,78 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
         child: Container(
-          height: 60,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          child: Column(
             children: [
-              CircleAvatar(
-                radius: 20,
-                backgroundImage: AssetImage('assets/profile.jpg'),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10, right: 100),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Hello Jixan,",
-                        style: TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.w500,
-                        )),
-                    Text(
-                      "Welcome Back!",
-                      style: TextStyle(fontSize: 17,
-                        fontWeight: FontWeight.bold,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: AssetImage('assets/profile.jpg'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 100),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Hello Jixan,",
+                            style: TextStyle(
+                              fontSize: 13,
+                              fontWeight: FontWeight.w500,
+                            )),
+                        Text(
+                          "Welcome Back!",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25),
                       ),
                     ),
-                  ],
-                ),
+                    child: Icon(
+                      Icons.notifications,
+                      size: 20,
+                      color: Colors.black54,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
               ),
               Container(
-                width: 40,
-                height: 40,
+                height: 45,
+                padding: EdgeInsets.only(top: 15, left: 20, bottom: 17),
                 decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(25),
-                  ),
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.grey[100],
                 ),
-                child: Icon(
-                  Icons.notifications,
-                  size: 20,color: Colors.black54,
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Search",
+                      hintStyle: TextStyle(fontSize: 14),
+                      icon: Icon(
+                        Icons.search,
+                        size: 20,
+                      ),
+                      border: InputBorder.none),
                 ),
-              )
+              ),
             ],
           ),
         ),

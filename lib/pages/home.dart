91b6d4f8d4e6ba2a5f30ccd8619f58903d6a39a9
zero0dayali <beeunik0@gmail.com>
2021-08-19 17:33:14 +0700

    add card

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -58,25 +58,78 @@ class Home extends StatelessWidget {
                   )
                 ],
               ),
-              SizedBox(
-                height: 10,
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+                child: Container(
+                  height: 45,
+                  padding: EdgeInsets.only(top: 15, left: 20, bottom: 17),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.grey[100],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(fontSize: 14),
+                        icon: Icon(
+                          Icons.search,
+                          size: 20,
+                        ),
+                        border: InputBorder.none),
+                  ),
+                ),
               ),
               Container(
-                height: 45,
-                padding: EdgeInsets.only(top: 15, left: 20, bottom: 17),
+                height: 150,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.grey[100],
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                      hintText: "Search",
-                      hintStyle: TextStyle(fontSize: 14),
-                      icon: Icon(
-                        Icons.search,
-                        size: 20,
+                    borderRadius: BorderRadius.circular(15),
+                    gradient: LinearGradient(
+                        colors: [Colors.blue, Color(0xff1565C0)])),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              "Total balance",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Text(
+                              "2,687.00",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
                       ),
-                      border: InputBorder.none),
+                      Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(25),
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.add,
+                          size: 20,
+                          color: Colors.blue,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ],

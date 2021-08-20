cc4c95f9677ab1e6225945e11387b031da35f347
zero0dayali <beeunik0@gmail.com>
2021-08-20 10:06:32 +0700

    add category card

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,7 +7,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+        padding: const EdgeInsets.only(top: 50, right: 15, left: 15),
         child: Container(
           child: Column(
             children: [
@@ -80,58 +80,169 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
-                height: 150,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    gradient: LinearGradient(
-                        colors: [Colors.blue, Color(0xff1565C0)])),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              "Total balance",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            Text(
-                              "2,687.00",
-                              style: TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: 40,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(25),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      gradient: LinearGradient(
+                          colors: [Colors.blue, Color(0xff1565C0)])),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                "Total balance",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Text(
+                                "2,687.00",
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ],
                           ),
                         ),
-                        child: Icon(
-                          Icons.add,
-                          size: 20,
-                          color: Colors.blue,
-                        ),
-                      )
-                    ],
+                        Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(25),
+                            ),
+                          ),
+                          child: Icon(
+                            Icons.add,
+                            size: 20,
+                            color: Colors.blue,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  childAspectRatio: .90,
+                  crossAxisSpacing: 25,
+                  mainAxisSpacing: 20,
+                  children: [
+                    CategoryCard(
+                        title: "Send \nMoney",
+                        subTitle: "To acc to acc",
+                        icon: Icon(
+                          Icons.send,
+                          color: Colors.orange,
+                        )),
+                    CategoryCard(
+                      title: "Receive Money",
+                      subTitle: "Manage Account",
+                      icon: Icon(
+                        Icons.inbox_sharp,
+                        color: Colors.green,
+                      ),
+                    ),
+                    CategoryCard(
+                        title: "Mobile \nPrepaid",
+                        subTitle: "Recharge Mobile",
+                        icon: Icon(
+                          Icons.send,
+                          color: Colors.orange,
+                        )),
+                    CategoryCard(
+                      title: "Bank to Bank",
+                      subTitle: "Send Money",
+                      icon: Icon(
+                        Icons.inbox_sharp,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  final Icon icon;
+  const CategoryCard({
+    Key? key,
+    required this.title,
+    required this.subTitle,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Color(0xfff5f5f5)),
+        boxShadow: [
+          BoxShadow(
+              offset: Offset(0, 17),
+              color: Colors.grey,
+              blurRadius: 17,
+              spreadRadius: -23)
+        ],
+        borderRadius: BorderRadius.circular(13),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {},
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.1),
+                      spreadRadius: 1,
+                      blurRadius: 2,
+                      offset: Offset(1, 3),
+                    )
+                  ],
+                ),
+                child: icon,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                ),
+              ),
+              Text(subTitle)
             ],
           ),
         ),
